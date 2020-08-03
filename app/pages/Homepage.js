98188d711ea9233aed99629cd18c4a613ed818e9
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-08-03 07:00:59 +0000

    audio upload

--- app/pages/Homepage.js
+++ app/pages/Homepage.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
 function Homepage() {
-  return <div className="py-5 max-w-6xl mx-auto">hi</div>;
+  async function handleGetUserAudio(e) {
+    const data = new FormData();
+    data.append('file', e.target.files[0]);
+    console.log(e.target.files[0]);
+
+    data.append('upload_preset', 'audio-uploads');
+    data.append('resource_type', 'video');
+
+    const res = await fetch(`https://api.cloudinary.com/v1_1/my-nigerian-projects/video/upload`, {
+      method: 'POST',
+      body: data,
+    });
+
+    const file = await res.json();
+
+    console.log(file.secure_url);
+  }
+  return (
+    <div className="py-5 max-w-6xl mx-auto text-center bg-white">
+      <form>
+        <input onChange={handleGetUserAudio} type="file" accept="audio/*" />
+      </form>
+    </div>
+  );
 }
 
 export default Homepage;
