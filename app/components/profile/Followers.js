c7bffbdaad38e8da3bebab9e2ebd98c50ea2c214
Adamu M. Dankore <zimmazone@yahoo.com>
2020-08-28 01:38:20 -0500

    Update files; followers features

--- app/components/profile/Followers.js
+++ app/components/profile/Followers.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import Axios from 'axios';
 import { useImmer } from 'use-immer';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+import Page from '../layouts/Page';
 
 function Followers() {
-  const [state] = useImmer({
+  const [state, setState] = useImmer({
     username: useParams().username,
+    followers: [],
   });
   useEffect(() => {
     const request = Axios.CancelToken.source();
@@ -16,7 +18,14 @@ function Followers() {
           CancelToken: request.token,
         });
 
-        console.log({ response: response.data });
+        if (response.data.status) {
+          setState(draft => {
+            draft.followers = response.data.followers;
+          });
+        } else {
+          // FAIL SILENTLY
+          console.log(response.data);
+        }
       })();
     } catch (error) {
       console.log(error);
@@ -24,7 +33,26 @@ function Followers() {
 
     return () => request.cancel();
   }, []);
-  return <>followers</>;
+
+  return (
+    <Page title="followers">
+      <div className="max-w-xl mx-auto bg-yellow-300">
+        {state.followers.map((follower, index) => {
+          return (
+            <Link
+              className="flex items-center"
+              key={index}
+              to={`/profile/${follower.author.username}`}
+            >
+              <img className="w-12 h-12 rounded-full mr-3" src={`${follower.author.avatar}`} />
+
+              <p>{follower.author.firstName}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </Page>
+  );
 }
 
 export default Followers;
