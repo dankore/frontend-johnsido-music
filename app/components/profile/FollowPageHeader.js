3cb85a27cd5c473f34194cbf61f453437cd52a4b
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-09-03 07:20:15 +0000

    refac

--- app/components/profile/FollowPageHeader.js
+++ app/components/profile/FollowPageHeader.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { activeNavCSS } from '../../helpers/CSSHelpers';
+import { activeNavCSS, linkCSS } from '../../helpers/CSSHelpers';
 
 function FollowPage({ profileUser }) {
   return (
-    <div className="flex justify-between p-3">
-      <NavLink activeStyle={activeNavCSS} to={`/profile/${profileUser.profileUsername}/followers`}>
-        Followers
-      </NavLink>
+    <>
+      <div className="flex justify-between py-3">
+        <Link className={linkCSS} to={`/profile/${profileUser.profileUsername}`}>
+          &lt;
+        </Link>{' '}
+        {profileUser.profileFirstName} {profileUser.profileLastName}{' '}
+        <Link className={linkCSS} to="/">
+          Home
+        </Link>
+      </div>
+      <div className="flex justify-between pb-2">
+        <NavLink
+          className={linkCSS}
+          activeStyle={activeNavCSS}
+          to={`/profile/${profileUser.profileUsername}/followers`}
+        >
+          Followers
+        </NavLink>
 
-      <NavLink activeStyle={activeNavCSS} to={`/profile/${profileUser.profileUsername}/following`}>
-        Following
-      </NavLink>
-    </div>
+        <NavLink
+          className={linkCSS}
+          activeStyle={activeNavCSS}
+          to={`/profile/${profileUser.profileUsername}/following`}
+        >
+          Following
+        </NavLink>
+      </div>
+    </>
   );
 }
 
