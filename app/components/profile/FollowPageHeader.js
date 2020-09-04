28a0710211334de95f491a794b171a4255a1589e
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-04 03:42:00 -0500

    Update files

--- app/components/profile/FollowPageHeader.js
+++ app/components/profile/FollowPageHeader.js
@@ -6,18 +6,24 @@ import { activeNavCSS, linkCSS, navLinkCSS } from '../../helpers/CSSHelpers';
 function FollowPage({ profileUser }) {
   return (
     <div className="px-2 lg:px-0">
-      <div className="flex justify-between py-3">
-        <Link className={linkCSS} to={`/profile/${profileUser.profileUsername}`}>
-          &lt;
-        </Link>{' '}
-        {profileUser.profileFirstName} {profileUser.profileLastName}{' '}
-        <Link className={linkCSS} to="/">
-          Home
-        </Link>
+      <div className="mt-2 mb-2">
+        <div className="flex items-center">
+          <Link className={linkCSS} to={`/profile/${profileUser.profileUsername}`}>
+            <svg viewBox="0 0 24 24" className="w-8 h-8" stroke="green" strokeWidth="0.5">
+              <g>
+                <path d="M20 11H7.414l4.293-4.293c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0l-6 6c-.39.39-.39 1.023 0 1.414l6 6c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L7.414 13H20c.553 0 1-.447 1-1s-.447-1-1-1z"></path>
+              </g>
+            </svg>
+          </Link>{' '}
+          <p className="font-bold ml-4">
+            {profileUser.profileFirstName} {profileUser.profileLastName}{' '}
+          </p>
+        </div>
+        <p className="ml-12 -m-2 text-gray-700">@{profileUser.profileUsername}</p>
       </div>
       <div className="flex justify-between pb-2">
         <NavLink
-          className={linkCSS + navLinkCSS}
+          className={linkCSS + navLinkCSS + ' hover:bg-green-300'}
           activeStyle={activeNavCSS}
           to={`/profile/${profileUser.profileUsername}/followers`}
         >
@@ -25,7 +31,7 @@ function FollowPage({ profileUser }) {
         </NavLink>
 
         <NavLink
-          className={linkCSS + navLinkCSS}
+          className={linkCSS + navLinkCSS + ' hover:bg-green-300'}
           activeStyle={activeNavCSS}
           to={`/profile/${profileUser.profileUsername}/following`}
         >
