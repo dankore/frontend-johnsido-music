9b6d0e148c41d416b79b59c76584e8fd05c42d45
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-09-08 03:54:59 +0000

    delete account set up

--- app/components/settings/DeleteAccount.js
+++ app/components/settings/DeleteAccount.js
@@ -12,12 +12,8 @@ function Container() {
           <div className="w-full sm:max-w-lg mx-auto p-3 bg-white">
             <div>
               <div className="flex items-start">
-                <div className="mx-auto">
-                  <i className="fas fa-exclamation-triangle mr-3"></i>
-                </div>
                 <div>
-                  <h2>Delete Account</h2>
-                  <p>
+                  <p className="mt-3 text-sm leading-5 text-gray-700">
                     Are you sure you want to delete your account? All of your data will be
                     permanently remove. This action cannot be undone.
                   </p>
@@ -25,20 +21,12 @@ function Container() {
               </div>
             </div>
             <div className="flex justify-end">
-              <div>
-                <button
-                  className="mr-5 px-4 py-2 border border-green-600 text-base leading-6 font-medium rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-                  type="submit"
-                >
-                  Cancel
-                </button>
-                <button
-                  className="px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md bg-red-600 text-white hover:bg-red-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-                  type="submit"
-                >
-                  Delete
-                </button>
-              </div>
+              <button
+                className="px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md bg-red-600 text-white hover:bg-red-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+                type="submit"
+              >
+                Delete
+              </button>
             </div>
           </div>
         </form>
