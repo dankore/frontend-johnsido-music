cd86d256b1d80990403290e1fa1122e6030da6ab
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-09-09 04:42:08 +0000

    update file

--- app/helpers/JSHelpers.js
+++ app/helpers/JSHelpers.js
@@ -2,24 +2,29 @@ module.exports = {
   timeAgo: function (previous) {
     var msPerMinute = 60 * 1000;
     var msPerHour = msPerMinute * 60;
-    var msPerDay = msPerHour * 24;
-    var msPerMonth = msPerDay * 30;
-    var msPerYear = msPerDay * 365;
+    var msPerDay = msPerHour * 24; //86,400,000
+    var msPerWeek = msPerDay * 7;
 
-    var elapsed = Date.now() - previous;
+    var elapsed = Date.now() - previous; // DATE.NOW() RETURNS NUMBER OF MILLISECONDS SINCE 1970.
+
+    elapsed = 584800000;
+
+    console.log(elapsed, Math.round(elapsed / msPerDay));
 
     if (elapsed < msPerMinute) {
       return Math.round(elapsed / 1000) + ' seconds ago';
-    } else if (elapsed < msPerHour) {
+    } else if (elapsed < msPerHour - msPerHour * 0.5) {
       return Math.round(elapsed / msPerMinute) + ' minutes ago';
-    } else if (elapsed < msPerDay) {
+    } else if (elapsed < msPerDay - msPerDay * 0.5) {
       return Math.round(elapsed / msPerHour) + ' hours ago';
-    } else if (elapsed < msPerMonth) {
-      return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
-    } else if (elapsed < msPerYear) {
-      return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
+    } else if (elapsed < msPerWeek - msPerWeek * 0.5) {
+      return Math.round(elapsed / msPerDay) == 1
+        ? ' one day ago'
+        : Math.round(elapsed / msPerDay) + ' days ago';
     } else {
-      return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
+      return Math.round(elapsed / msPerWeek) == 1
+        ? ' one week ago'
+        : Math.round(elapsed / msPerWeek) + ' weeks ago';
     }
   },
   formatTitleAndDescription: function (s) {
