82d59de54e5dcfc8a3135560f167f2afec2067b4
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-10 07:59:19 -0500

    update file

--- app/pages/admin/AdminLandingPage.js
+++ app/pages/admin/AdminLandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Page from '../../components/layouts/Page';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Route, Switch, Redirect } from 'react-router-dom';
+import Analytics from '../../components/admin/Analytics';
 
 function AdminLandingPage() {
   return (
@@ -23,7 +24,7 @@ function AdminLandingPage() {
           <li className="flex-1 md:flex-none md:mr-3">
             <div className="relative inline-block">
               <button
-                onClick="toggleDD('myDropdown')"
+                // onClick="toggleDD('myDropdown')"
                 className="drop-button text-white focus:outline-none"
               >
                 {' '}
@@ -73,7 +74,7 @@ function AdminLandingPage() {
           <div className="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
             <ul className="flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
               <NavLink
-                to="/"
+                to="/admin/analytics"
                 className="mr-3 flex-1 block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500"
               >
                 <i className="fas fa-tasks md:mr-3"></i>
@@ -82,7 +83,7 @@ function AdminLandingPage() {
                 </span>
               </NavLink>
               <NavLink
-                to="/"
+                to="/admin/add-song"
                 className="mr-3 flex-1 block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500"
               >
                 <i className="fa fa-envelope md:mr-3"></i>
@@ -93,124 +94,16 @@ function AdminLandingPage() {
             </ul>
           </div>
         </div>
-
         <div className="main-content flex-1 bg-gray-100 mt-10 pb-24 md:pb-5">
-          <div className="bg-blue-800 px-2 pt-6 pb-4 shadow text-xl text-white">
-            <h3 className="font-bold pl-2">Analytics</h3>
-          </div>
-
-          <div className="flex flex-wrap">
-            <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-              <div className="bg-green-100 border-b-4 border-green-600 rounded-lg shadow-lg p-5">
-                <div className="flex flex-row items-center">
-                  <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-green-600">
-                      <i className="fa fa-wallet fa-2x fa-inverse"></i>
-                    </div>
-                  </div>
-                  <div className="flex-1 text-right md:text-center">
-                    <h5 className="font-bold uppercase text-gray-600">Total Revenue</h5>
-                    <h3 className="font-bold text-3xl">
-                      $3249{' '}
-                      <span className="text-green-500">
-                        <i className="fas fa-caret-up"></i>
-                      </span>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-              <div className="bg-orange-100 border-b-4 border-orange-500 rounded-lg shadow-lg p-5">
-                <div className="flex flex-row items-center">
-                  <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-orange-600">
-                      <i className="fas fa-users fa-2x fa-inverse"></i>
-                    </div>
-                  </div>
-                  <div className="flex-1 text-right md:text-center">
-                    <h5 className="font-bold uppercase text-gray-600">Total Users</h5>
-                    <h3 className="font-bold text-3xl">
-                      249{' '}
-                      <span className="text-orange-500">
-                        <i className="fas fa-exchange-alt"></i>
-                      </span>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-              <div className="bg-yellow-100 border-b-4 border-yellow-600 rounded-lg shadow-lg p-5">
-                <div className="flex flex-row items-center">
-                  <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-yellow-600">
-                      <i className="fas fa-user-plus fa-2x fa-inverse"></i>
-                    </div>
-                  </div>
-                  <div className="flex-1 text-right md:text-center">
-                    <h5 className="font-bold uppercase text-gray-600">New Users</h5>
-                    <h3 className="font-bold text-3xl">
-                      2{' '}
-                      <span className="text-yellow-600">
-                        <i className="fas fa-caret-up"></i>
-                      </span>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-              <div className="bg-blue-100 border-b-4 border-blue-500 rounded-lg shadow-lg p-5">
-                <div className="flex flex-row items-center">
-                  <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-blue-600">
-                      <i className="fas fa-server fa-2x fa-inverse"></i>
-                    </div>
-                  </div>
-                  <div className="flex-1 text-right md:text-center">
-                    <h5 className="font-bold uppercase text-gray-600">Server Uptime</h5>
-                    <h3 className="font-bold text-3xl">152 days</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-              <div className="bg-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-lg p-5">
-                <div className="flex flex-row items-center">
-                  <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-indigo-600">
-                      <i className="fas fa-tasks fa-2x fa-inverse"></i>
-                    </div>
-                  </div>
-                  <div className="flex-1 text-right md:text-center">
-                    <h5 className="font-bold uppercase text-gray-600">To Do List</h5>
-                    <h3 className="font-bold text-3xl">7 tasks</h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full md:w-1/2 xl:w-1/3 p-3">
-              <div className="bg-red-100 border-b-4 border-red-500 rounded-lg shadow-lg p-5">
-                <div className="flex flex-row items-center">
-                  <div className="flex-shrink pr-4">
-                    <div className="rounded-full p-5 bg-red-600">
-                      <i className="fas fa-inbox fa-2x fa-inverse"></i>
-                    </div>
-                  </div>
-                  <div className="flex-1 text-right md:text-center">
-                    <h5 className="font-bold uppercase text-gray-600">Issues</h5>
-                    <h3 className="font-bold text-3xl">
-                      3{' '}
-                      <span className="text-red-500">
-                        <i className="fas fa-caret-up"></i>
-                      </span>
-                    </h3>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Switch>
+            <Route path="/admin/analytics">
+              <Analytics />
+              {/* <div className="bg-red-400 w-full mt-64 tex-4xl">Adamu</div> */}
+            </Route>
+            <Route path="/admin">
+              <Redirect to="/admin/analytics" />
+            </Route>
+          </Switch>
         </div>
       </div>
     </Page>
