0953b6996bd173b293f6217fe7d984176a738484
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-15 07:00:29 -0500

    update file

--- app/pages/LandingPage.js
+++ app/pages/LandingPage.js
@@ -91,7 +91,7 @@ function LandingPage() {
                                 />
                                 <p>Your Profile</p>
                               </Link>
-                              {appState.user && appState.user.scope.indexOf('admin') > -1 && (
+                              {appState.user.scope && appState.user.scope.indexOf('admin') > -1 && (
                                 <Link
                                   to={`/admin/${appState.user.username}`}
                                   className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
