fcc768daa195c1303266e6c28341003b74522534
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-09-17 04:20:11 +0000

    fix html issue

--- app/components/shared/UserNotActive.js
+++ app/components/shared/UserNotActive.js
@@ -3,24 +3,24 @@ import PropTypes from 'prop-types';
 
 function UserNotActive({ user }) {
   return (
-    <div className="w-full -mt-56">
-      <div className="flex justify-center">
+    <div className="w-full">
+      <div className="flex justify-center relative break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-24">
         <img
           alt="Profile avatar"
           src={user.profileAvatar}
-          className="shadow-xl rounded-full h-auto align-middle border-none"
+          className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
           style={{ maxWidth: '150px' }}
         />
       </div>
 
-      <div className="text-center">
+      <div className="text-center mt-24">
         <h3 className="text-4xl font-semibold leading-normal text-gray-800">
           {user.profileFirstName} {user.profileLastName}
         </h3>
         <p className="mb-2">@{user.profileUsername}</p>
       </div>
       <h2 className="text-center text-2xl px-3">Deactivated Account</h2>
-      <h2 className="mt-2 px-3 max-w-lg mx-auto text-2xl leading-8 font-semibold font-display text-gray-600 sm:text-3xl sm:leading-9 lg:text-4xl lg:leading-10 text-center">
+      <h2 className="mt-2 px-3 max-w-lg mx-auto leading-8 font-semibold font-display text-gray-600 sm:leading-9 lg:leading-10 text-center">
         {user.profileFirstName} {user.profileLastName}&apos;s account has been deactivated by this
         website&apos;s admin.
       </h2>
