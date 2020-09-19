a71156436d8e440a9be51c80d121efe5dbec1845
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-19 02:55:11 -0500

    update search css

--- app/components/admin/RoleAssignment.js
+++ app/components/admin/RoleAssignment.js
@@ -266,34 +266,13 @@ function RoleAssignment({ history }) {
             <div className="text-center w-full md:max-w-md mb-5 md:mx-3">
               <p className="text-2xl">Click to edit roles</p>
               {/* SEARCH */}
-              <div className="flex flex-1 justify-center text-white mt-5 preventAutoZoom">
-                <span className="relative w-full">
-                  <input
-                    onChange={e =>
-                      roleAssignmentDispatch({ type: 'search', value: e.target.value })
-                    }
-                    autoComplete="off"
-                    type="search"
-                    placeholder="Search"
-                    className="w-full bg-gray-900 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 py-1 px-2 pl-10 appearance-none leading-normal"
-                  />
-                  <div
-                    className="absolute"
-                    style={{
-                      top: 0.5 + 'rem',
-                      left: 0.8 + 'rem',
-                    }}
-                  >
-                    <svg
-                      className="fill-current pointer-events-none text-white w-4 h-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
-                    </svg>
-                  </div>
-                </span>
-              </div>
+              <input
+                onChange={e => roleAssignmentDispatch({ type: 'search', value: e.target.value })}
+                autoComplete="off"
+                type="search"
+                placeholder="Search"
+                className="w-full mt-5 preventAutoZoom bg-gray-900 text-sm text-white transition border border-transparent focus:outline-none focus:border-gray-700 py-1 px-2 pl-10 appearance-none leading-normal"
+              />
               {state.search.loading && <div className="absolute">Searching...</div>}
             </div>
 
