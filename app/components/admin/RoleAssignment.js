9d425a3fc9ec3d00b0b9c8740770127cd551dbc1
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-20 02:52:10 -0500

    update role file

--- app/components/admin/RoleAssignment.js
+++ app/components/admin/RoleAssignment.js
@@ -269,7 +269,7 @@ function RoleAssignment({ history }) {
               <input
                 onChange={e => roleAssignmentDispatch({ type: 'search', value: e.target.value })}
                 autoComplete="off"
-                type="search"
+                type="text"
                 placeholder="Search"
                 className="w-full mt-5 preventAutoZoom bg-gray-900 text-sm text-white focus:outline-none py-1 px-2 appearance-none leading-normal"
               />
