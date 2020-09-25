9d49e1fca8614a32170cb110e134c4bffc759313
Adamu M. Dankore <zimmazone@yahoo.com>
2020-09-25 05:09:10 -0500

    Upload song feature

--- app/pages/admin/AdminLandingPage.js
+++ app/pages/admin/AdminLandingPage.js
@@ -131,8 +131,8 @@ function AdminLandingPage({ history }) {
         </ul>
       </nav>
 
-      <div className="flex flex-col md:flex-row">
-        <div className="bg-gray-900 shadow-lg h-16 fixed bottom-0 md:relative md:h-screen z-10 w-full md:w-48">
+      <div className="flex flex-col md:flex-row bg-gray-900">
+        <div className="shadow-lg h-16 fixed bottom-0 md:relative md:h-screen z-10 w-full md:w-48">
           <div className="md:mt-12 md:w-48 md:fixed md:left-0 md:top-0 content-center md:content-start text-left justify-between">
             <ul className="flex flex-row md:flex-col py-0 md:py-3 px-1 md:px-2 text-center md:text-left">
               <NavLink
