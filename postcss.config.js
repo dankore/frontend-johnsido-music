6cd5cb55c4d4c21e481c60a49fee1979c47893e4
Adamu M. Dankore <zimmazone@yahoo.com>
2020-10-07 17:45:50 -0500

    dev purge css config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
-// const PurgeCss = require('@fullhuman/postcss-purgecss');
-// const PurgeOptions = {
-//   // SPECIFY PATH THAT CONTAINS CSS
-//   content: ['./app/**/*.html', './app/**/*.js'],
+const PurgeCss = require('@fullhuman/postcss-purgecss');
+const PurgeOptions = {
+  // SPECIFY PATH THAT CONTAINS CSS
+  content: ['./app/**/*.html', './app/**/*.js'],
 
-//   // Include any special characters you're using in this regular expression
-//   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-// };
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+};
+const DEV = process.env.npm_lifecycle_event !== 'build';
 
 module.exports = {
   plugins: [
     //
     require('tailwindcss'),
     require('autoprefixer'),
-    // PurgeCss(PurgeOptions),
+    DEV ? false : PurgeCss(PurgeOptions),
     //
-  ],
+  ].filter(Boolean),
 };
