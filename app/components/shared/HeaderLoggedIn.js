dd9e9c8059b5a6af2deb691a9edb9335d77833e6
Adamu M. Dankore <zimmazone@yahoo.com>
2020-10-11 04:41:58 -0500

    nav link

--- app/components/shared/HeaderLoggedIn.js
+++ app/components/shared/HeaderLoggedIn.js
@@ -132,14 +132,17 @@ function HeaderLoggedIn({ history }) {
                     aria-orientation="vertical"
                     aria-labelledby="user-menu"
                   >
-                    <Link
-                      to={`/profile/${appState.user.username}`}
-                      className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-                      role="menuitem"
-                    >
-                      <i className="mr-2 fa fa-user fa-fw"></i>
-                      Your Profile
-                    </Link>
+                    {history.location.pathname != `/profile/${appState.user.username}` && (
+                      <Link
+                        to={`/profile/${appState.user.username}`}
+                        className="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
+                        role="menuitem"
+                      >
+                        <i className="mr-2 fa fa-user fa-fw"></i>
+                        Your Profileg
+                      </Link>
+                    )}
+
                     {appState.user.scope.indexOf('admin') > -1 && (
                       <Link
                         to={`/admin/${appState.user.username}`}
