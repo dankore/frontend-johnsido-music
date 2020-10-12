5c137a069a7cc948503bcdcfc751b650e0146644
Adamu M. Dankore <zimmazone@yahoo.com>
2020-10-12 08:14:17 -0500

    CSS, undo

--- app/components/admin/ReuseableModal.js
+++ app/components/admin/ReuseableModal.js
@@ -49,7 +49,7 @@ function ReuseableModal({
 
   return (
     <div className="fixed inset-0 z-10 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-center justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
         <div onClick={handleToggle} className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
