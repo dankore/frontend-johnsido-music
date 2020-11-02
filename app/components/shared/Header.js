0d85b8593a027a54e4dc08b02e3b787b8109623a
Adamu M. Dankore <zimmazone@yahoo.com>
2020-11-02 04:36:00 -0600

    Update files

--- app/components/shared/Header.js
+++ app/components/shared/Header.js
@@ -2,10 +2,16 @@ import React, { useContext } from 'react';
 import HeaderLoggedIn from './HeaderLoggedIn';
 import HeaderLoggedOut from './HeaderLoggedOut';
 import StateContext from '../../contextsProviders/StateContext';
+import Welcome from './Welcome';
 
 function Header() {
   const appState = useContext(StateContext);
-  return <>{appState && appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}</>;
+  return (
+    <div>
+      <Welcome />
+      {appState && appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+    </div>
+  );
 }
 
 export default Header;
