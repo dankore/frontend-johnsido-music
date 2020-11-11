ccfbf1270cf2cdcb29238af566571e371f5c48a9
dankore <zimmazone@yahoo.com>
2020-11-11 06:59:31 +0000

    update files

--- app/pages/auth/ResetPasswordStep1.js
+++ app/pages/auth/ResetPasswordStep1.js
@@ -120,7 +120,7 @@ function ResetPasswordStep1() {
             process: state.usernameOrEmail.type,
           });
         } catch (error) {
-          console.log('Having difficulty looking up your email. Please try again.');
+          console.log('Having difficulty looking up your account. Please try again.');
         }
       })();
 
@@ -201,76 +201,84 @@ function ResetPasswordStep1() {
           <p className="text-center text-lg mb-4 px-3">Step 1 of 2:</p>
           {/* FORM */}
           <form onSubmit={handleSubmit} className="flex flex-col p-3 w-full sm:max-w-md mx-auto">
-            {/* EMAIL */}
-            <div className="relative flex flex-col">
-              {state.showNextStep && (
-                <div
-                  style={{ backgroundColor: 'rgba(19, 84, 122, 1)' }}
-                  className="absolute z-10 flex items-center text-white text-sm font-bold px-4 py-3"
+            {/* LABEL AND INPUT */}
+            {!state.showNextStep && (
+              <div>
+                <div className="flex flex-col">
+                  <label className="text-lg" htmlFor="email-or-username">
+                    Enter Your Username or Email <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    onChange={e =>
+                      resetPasswordStep1Dispatch({
+                        type: 'emailImmediately',
+                        value: e.target.value,
+                      })
+                    }
+                    id="email-or-username"
+                    autoComplete="off"
+                    className="rounded w-full px-3 py-2 leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
+                    type="text"
+                  />
+                  <CSSTransition
+                    in={state.usernameOrEmail.hasErrors}
+                    timeout={330}
+                    className="liveValidateMessage"
+                    unmountOnExit
+                  >
+                    <div style={CSSTransitionStyle} className="liveValidateMessage">
+                      {state.usernameOrEmail.message}
+                    </div>
+                  </CSSTransition>
+                </div>
+                {/* SUBMIT BTN */}
+                <button
+                  type="submit"
+                  className="w-full p-2 mt-8 text-lg font-bold text-white bg-black hover:bg-gray-700"
+                >
+                  {state.isLoggingIn ? (
+                    <span>
+                      <i className="text-sm fa fa-spinner fa-spin"></i>
+                    </span>
+                  ) : (
+                    <>Login In</>
+                  )}
+                </button>
+              </div>
+            )}
+            {/* SUCCESS MESSAGE */}
+            {state.showNextStep && (
+              <div
+                style={{ backgroundColor: 'rgba(19, 84, 122, 1)' }}
+                className="relative flex items-center text-white text-sm font-bold px-4 py-3"
+              >
+                <svg
+                  className="fill-current w-20 h-20 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+                </svg>
+                <p>
+                  Check your email inbox for further instruction. Check your SPAM folder if you
+                  cannot locate the email in your regular inbox.
+                </p>
+                <span
+                  onClick={() => resetPasswordStep1Dispatch({ type: 'closeAlert' })}
+                  className="absolute top-0 right-0 px-4 py-3"
                 >
                   <svg
-                    className="fill-current w-20 h-20 mr-2"
+                    className="fill-current h-6 w-6 text-red-500"
+                    role="button"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
-                    <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+                    <title>Close</title>
+                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
                   </svg>
-                  <p>
-                    Check your email inbox for further instruction. Check your SPAM folder if you
-                    cannot locate the email in your regular inbox.
-                  </p>
-                  <span
-                    onClick={() => resetPasswordStep1Dispatch({ type: 'closeAlert' })}
-                    className="absolute top-0 bottom-0 right-0 px-4 py-3"
-                  >
-                    <svg
-                      className="fill-current h-6 w-6 text-red-500"
-                      role="button"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <title>Close</title>
-                      <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-                    </svg>
-                  </span>
-                </div>
-              )}
-              <label className="text-lg" htmlFor="email-or-username">
-                Enter Your Username or Email <span className="text-red-600">*</span>
-              </label>
-              <input
-                onChange={e =>
-                  resetPasswordStep1Dispatch({ type: 'emailImmediately', value: e.target.value })
-                }
-                id="email-or-username"
-                autoComplete="off"
-                className="rounded w-full px-3 py-2 leading-tight text-gray-700 border shadow appearance-none focus:outline-none focus:shadow-outline"
-                type="text"
-              />
-              <CSSTransition
-                in={state.usernameOrEmail.hasErrors}
-                timeout={330}
-                className="liveValidateMessage"
-                unmountOnExit
-              >
-                <div style={CSSTransitionStyle} className="liveValidateMessage">
-                  {state.usernameOrEmail.message}
-                </div>
-              </CSSTransition>
-            </div>
-            {/* SUBMIT BTN */}
-            <button
-              type="submit"
-              className="p-2 mt-8 text-lg font-bold text-white bg-black hover:bg-gray-700"
-            >
-              {state.isLoggingIn ? (
-                <span>
-                  <i className="text-sm fa fa-spinner fa-spin"></i>
                 </span>
-              ) : (
-                <>Login In</>
-              )}
-            </button>
+              </div>
+            )}
           </form>
           <div className="pt-12 pb-12 text-center">
             <p>
